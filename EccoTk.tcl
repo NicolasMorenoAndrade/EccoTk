030d366870356c0dfcbbd51fb282a0a3359acccd
NicolasMoreno <nicolas@eccole.co>
2016-06-16 11:17:30 -0500

    added autocompcombo.R : EccoTk usage within R scripts

--- EccoTk.tcl
+++ EccoTk.tcl
@@ -26,26 +26,41 @@ proc ::EccoTk::GetPath {} {
 }
 
 proc ::EccoTk::ComboBoxAutoComplete {path key} {
-        # autocomplete a string in the ttk::combobox from the list of values
-        #-
-        # Any key string with more than one character and is not entirely
-        # lower-case is considered a function key and is thus ignored.
-        #
-        # path -> path to the combobox
-        #
-        if {[string length $key] > 1 && [string tolower $key] != $key} {return}
-        
+
+
+
+	# Any key string with more than one character is considered a function key and is thus ignored
+
+	if {[string length $key] > 1} {return}
+
+
+	# disabling [ and ]
         set text [string map [list {[} {\[} {]} {\]}] [$path get]]
-        if {[string equal $text ""]} {return}
-        
+
         set values [$path cget -values]
-        set x [lsearch $values $text*]
-        if {$x < 0} {return}
-        
-        set index [$path index insert]
-        $path set [lindex $values $x]
-        $path icursor $index
-        $path selection range insert end
+
+
+	# hace la b´usqueda de los strings en la lista values que empiezan por lo escrito ya sea al principio de la palabra o 		despu´es de un espacio, pero primero se asegura de que haya un match
+
+
+	if {[lsearch -regexp -nocase $values [subst -nocommands -nobackslashes {(\A$text)|(\s$text)}]]<0} {return} 
+        set actualizados [lsearch -regexp -nocase -all -inline $values [subst -nocommands -nobackslashes {(\A$text)|(\s$text)}]]
+
+	$path configure -values $actualizados
+	
+	set prm [lindex $actualizados 0]
+
+	if {[string compare [string toupper $text] $prm]<0} {set prol [string length $text]}
+
+
+	set fir [expr [string first [string toupper " $text"] $prm] - 1]
+	set prol [expr [string first [string toupper " $text"] $prm] + [string length $text] + 1]
+	$path set $prm
+	$path icursor $prol
+	$path selection range $prol end
+
+
+	
 }
 
 package provide EccoTk $EccoTk::version
