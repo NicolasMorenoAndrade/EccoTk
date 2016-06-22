a01ab6d48f2de5e6cffe6c991caa4315cf85546b
Ana Hernández Andrade <anahernandezandrade@Ana-Hernandez-Andrades-MacBook-Pro.local>
2016-06-22 11:54:11 -0500

    remplace el procedimiento ComboBoxAutocomplete por uno actualizado

--- EccoTk.tcl
+++ EccoTk.tcl
@@ -28,7 +28,7 @@ proc ::EccoTk::GetPath {} {
 }
 
 # Busca en la lista y la actualiza segun lo encontrado
-proc buscarActualizar {path} {
+proc ::EccoTk::buscarActualizar {path} {
 
 	set text [$path get]
 	variable listaColegios $listaColegios
@@ -44,38 +44,24 @@ proc buscarActualizar {path} {
 proc ::EccoTk::ComboBoxAutoComplete {path key} {
 
 
-	# Any key string with more than one character is considered a function key and is thus ignored
-	if {[string length $key] > 1} {return}
+	variable listaColegios $EccoTk::listaColegios
+	$path configure -values $listaColegios	
 
+	if {[string length $key] > 1} {return}
 	
 	# disabling [ and ]
-	
         set text [string map [list {[} {\[} {]} {\]}] [$path get]]
-	
-	#fix backspace??
-	if {[string equal $text "BackSpace"]} {return}
 
-	if {[string equal $text ""]} {return}
-	
-        set values [$path cget -values]
-
-
-	# hace la b´usqueda de los strings en la lista values que empiezan por lo escrito ya sea al principio de la palabra o 		despu´es de un espacio, pero primero se asegura de que haya un match
-	if {[lsearch -regexp -nocase $values [subst -nocommands -nobackslashes {(\A$text)|(\s$text)}]]<0} {return} 
-        
-	set actualizados [lsearch -regexp -nocase -all -inline $values [subst -nocommands -nobackslashes {(\A$text)|(\s$text)}]]
-
-	$path configure -values $actualizados
-	
+	set actualizados [EccoTk::buscarActualizar $path]
 	set prm [lindex $actualizados 0]
 
 	if {[string compare [string toupper $text] $prm]<0} {set prol [string length $text]}
 
+	set fir [expr [string first [string toupper " $text"] $prm] - 1]
 	set prol [expr [string first [string toupper " $text"] $prm] + [string length $text] + 1]
 	$path set $prm
 	$path icursor $prol
 	$path selection range $prol end
-
 	
 }
 
